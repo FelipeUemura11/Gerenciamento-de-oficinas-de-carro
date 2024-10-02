3f8e892a92d638abe26b75279e49098ef9beac03
Felipe <felipe.uemura11@gmail.com>
2024-10-02 17:23:34 -0300

    Install, react com type scripts

---
+++ GerenciamentoAutomoveis/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+//1 - Um componente SEMPRE deve começar com a primeira letra maiúscula
+//2 - Todo componente DEVE ser uma função do JS
+//3 - Todo deve retornar apenas UM elemento HTML
+function App() {
+  return (
+    <div>
+      <h1>Projeto base em React com TypeScript</h1>
+    </div>
+  );
+}
+//4 - OBRIGATORIAMENTE o componente DEVE ser exportado
+export default App;
