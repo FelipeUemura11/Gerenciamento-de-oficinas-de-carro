4903bc94ca1e67597818997d59f337dc41732d9b
Icaro Gabriel <icarogabi10@gmail.com>
2024-12-01 16:09:09 -0300

    Script de Inicialização

--- AutoManagerAPI/startup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting .NET project...
-start dotnet run
-
-echo Starting React project...
-cd apireact
-start npm start
---
+++ startup.bat
@@ -0,0 +1,5 @@
+REM Navegar para a pasta AutoManagerAPI e executar dotnet run em um novo terminal PowerShell
+start powershell -NoExit -Command "cd AutoManagerAPI; dotnet run"
+
+REM Navegar para a pasta front e executar npm start em um novo terminal PowerShell
+start powershell -NoExit -Command "cd front; npm start"
